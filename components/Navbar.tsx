09b48e1f75cd0963a2fe642854b1e00e640cba76
Muaaz Chihabi <73114423+MuaazChihabiMMC@users.noreply.github.com>
2025-05-01 21:52:38 +0200

    editnavbar white not showing

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ import MobileMenu from './MobileMenu';
 
 const Navbar = () => {
   return (
-    <nav className="flexBetween max-container padding-container relative z-30 py-5">
+    <nav className="flexBetween max-container padding-container relative z-30 py-5 bg-white shadow-sm">
       <Link href="/">
         <Image src="/cait logo tran black.svg" alt="logo" width={100} height={29} />
       </Link>
@@ -16,30 +16,27 @@ const Navbar = () => {
         {NAV_LINKS.map((link) =>
           link.children ? (
             <li key={link.key} className="relative group px-2 pb-2">
-
-
-            <span className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
-              {link.label} ▾
-            </span>
-            <ul className="absolute top-full left-0 hidden group-hover:flex flex-col bg-white text-gray-800 shadow-lg rounded-md py-2 w-56 z-50 pointer-events-auto min-h-[80px]">
-
-              {link.children.map((child) => (
-                <li key={child.key}>
-                  <Link
-                    href={child.href}
-                    className="block px-4 py-2 hover:bg-gray-100"
-                  >
-                    {child.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </li>
+              <span className="regular-16 text-gray-800 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold hover:text-blue-600">
+                {link.label} ▾
+              </span>
+              <ul className="absolute top-full left-0 hidden group-hover:flex flex-col bg-white text-gray-800 shadow-lg rounded-md py-2 w-56 z-50 pointer-events-auto min-h-[80px] border border-gray-100">
+                {link.children.map((child) => (
+                  <li key={child.key}>
+                    <Link
+                      href={child.href}
+                      className="block px-4 py-2 hover:bg-blue-50 hover:text-blue-600 transition-colors"
+                    >
+                      {child.label}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </li>
           ) : (
             <li key={link.key}>
               <Link
                 href={link.href}
-                className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+                className="regular-16 text-gray-800 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold hover:text-blue-600"
               >
                 {link.label}
               </Link>
@@ -48,7 +45,14 @@ const Navbar = () => {
         )}
       </ul>
 
-     
+      <div className="lg:flexCenter hidden">
+        <Button 
+          type="button"
+          title="Kontakt"
+          icon="/user.svg"
+          variant="btn_dark_green"
+        />
+      </div>
 
       <Image
         src="menu.svg"
