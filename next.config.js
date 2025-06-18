165681daa54071550bff1d6731f760765c6c74e3
Muaaz Chihabi <73114423+MuaazChihabiMMC@users.noreply.github.com>
2025-06-18 20:37:06 +0200

    seo zu seo-agentur-berlin3

--- next.config.js
+++ next.config.js
@@ -32,3 +32,50 @@ module.exports = {
     ];
   },
 };
+
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/seo',
+        destination: '/seo-agentur-berlin',
+        permanent: true,
+      },
+      {
+        source: '/seo/content-erstellung',
+        destination: '/seo-agentur-berlin/content-erstellung',
+        permanent: true,
+      },
+      {
+        source: '/seo/keyword-recherche',
+        destination: '/seo-agentur-berlin/keyword-recherche',
+        permanent: true,
+      },
+      {
+        source: '/seo/lokale-seo',
+        destination: '/seo-agentur-berlin/lokale-seo',
+        permanent: true,
+      },
+      {
+        source: '/seo/monitoring-reporting',
+        destination: '/seo-agentur-berlin/monitoring-reporting',
+        permanent: true,
+      },
+      {
+        source: '/seo/offpage-seo',
+        destination: '/seo-agentur-berlin/offpage-seo',
+        permanent: true,
+      },
+      {
+        source: '/seo/onpage-seo',
+        destination: '/seo-agentur-berlin/onpage-seo',
+        permanent: true,
+      },
+      {
+        source: '/seo/technisches-seo',
+        destination: '/seo-agentur-berlin/technisches-seo',
+        permanent: true,
+      },
+    ];
+  },
+};
